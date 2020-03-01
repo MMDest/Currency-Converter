a86875293e524bf32f116bff9554d83c2973c97a
MineDest <mdest@MacBook-Pro-MineDest.local>
2020-03-01 22:26:40 +0200

    Connect Api to TableView

---
+++ Currency Converter/CustomTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CustomTableViewCell.swift
+//  Currency Converter
+//
+//  Created by MineDest on 3/1/20.
+//  Copyright © 2020 MineDest. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyCountryLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
