02c3153cb964e565d2116f35b87915c2af0bff51
MineDest <mdest@MacBook-Pro-MineDest.local>
2020-03-03 19:03:40 +0200

    Add segue to ChangeCurrencyVC

--- Currency Converter/ViewController.swift
+++ Currency Converter/ViewController.swift
@@ -10,10 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
+    @IBOutlet weak var firstLabel: UITextField!
+    @IBOutlet weak var secondLabel: UITextField!
+    
+    var first:String = "EUR"
+    var second:String = "USD"
+    var firstValue:Double = 0;
+    var secondValue:Double = 0;
+    var choose:Bool?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        firstButton.setTitle(first, for: .normal)
+        secondButton.setTitle(second, for: .normal)
+        
     }
 
+
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "firstButtonSegue"{
+            let dvc = segue.destination as? ChangeCurrencyVC
+            dvc?.choose = true
+        } else if segue.identifier == "secondButtonSegue" {
+            let dvc = segue.destination as? ChangeCurrencyVC
+            dvc?.choose = false
+        }
+    }
+    
+    @IBAction func ChangeToOpposite(_ sender: UIButton) {
+    }
+    
+   
+    @IBAction func ConvertButton(_ sender: UIButton) {
+        
+        	
+        
+    }
+    
 }
 
